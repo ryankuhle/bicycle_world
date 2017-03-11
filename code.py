3c7e793ad817999cd174b0e6179e902e233d015c
Ryan Kuhle <ryan@ryankuhle.com>
2017-03-11 00:04:08 +0000

    Create base Class structure

---
+++ code.py
@@ -0,0 +1,11 @@
+class Bicycle(object):
+    def __init__(self):
+        pass
+        
+class BikeShops(object):
+    def __init__(self):
+        pass
+
+class Customers(object):
+    def __init__(self):
+        pass
